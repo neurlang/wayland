10c6f0e1c8d5e39cf98e45fc4519c1573d4efacb
neurlang <77860779+neurlang@users.noreply.github.com>
2025-11-25 08:12:37 +0100

    Fix (xkbcommon): Add keysyms for wine

--- xkbcommon/keysyms_windows.go
+++ xkbcommon/keysyms_windows.go
@@ -14,5 +14,24 @@ const KeyControl = 0x1d
 const KeyControlL uint32 = 17 /* Left control */
 const KeyControlR uint32 = 17 /* Right control */
 
+const KeyTab = 983041
+const KeyEscape = 65537
+const KeyPageUp = 21561345
+const KeyPageDown = 22085633
+const KeyInsert = 22151169
+const KeyF1 = 3866625
+const KeyF2 = 3932161
+const KeyF3 = 3997697
+const KeyF4 = 4063233
+const KeyF5 = 4128769
+const KeyF6 = 4194305
+const KeyF7 = 4259841
+const KeyF8 = 4325377
+const KeyF9 = 4390913
+const KeyF10 = 9999999 /* can't be able to be pressed on wine */
+const KeyF11 = 5701633
+const KeyF12 = 5767169
+
+
 const KeyQ = 81
 const KEYq = 81
